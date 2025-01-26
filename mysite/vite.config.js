24890fe320ea20888c86370457aacfef10402bd7
AlibekovNurtilek <viruskg007@gmail.com>
2025-01-26 06:35:59 +0600

    vite.confgi update

--- mysite/vite.config.js
+++ mysite/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://https://3.84.57.147:8000', // Замените <your-fastapi-server> на адрес вашего FastAPI-сервера
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Удаляет префикс '/api' из запроса
+      },
+    },
+  },
+});
