e9c9d738a5953dc157a757faa7ae3e9f00fa9bfc
AlibekovNurtilek <nurtilek007007@gmail.com>
2025-06-18 13:44:33 +0600

    f

--- mysite/vite.config.js
+++ mysite/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  
-  // ВАЖНО: гарантирует, что пути в index.html будут корректны (особенно для продакшна)
-  base: '/',
-
-  // Прокси только для dev-сервера
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://3.84.57.147:8000',  // 👈 исправленный адрес FastAPI
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // удаляем /api перед запросом
-      },
-    },
-  },
-});
+  base: '/', // <<< ВАЖНО!
+})
